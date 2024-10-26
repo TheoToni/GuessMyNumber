80ddd147640c106c40eeb016d5e531e27150f014
TheoToni <32997269+TheoToni@users.noreply.github.com>
2024-10-26 11:03:50 +0200

    Styling

--- components/PrimaryButton.js
+++ components/PrimaryButton.js
@@ -1,11 +1,29 @@
-import { Text, View, StyleSheet } from "react-native";
+import { Text, View, StyleSheet, Pressable } from "react-native";
 
 export default function PrimaryButton({ children }) {
+  function pressHandler() {
+    console.log("pressed");
+  }
   return (
-    <View>
-      <Text>{children}</Text>
-    </View>
+    <Pressable onPress={pressHandler}>
+      <View style={styles.container}>
+        <Text style={styles.buttonText}>{children}</Text>
+      </View>
+    </Pressable>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#72063c",
+    borderRadius: 28,
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+    elevation: 2,
+    margin: 4,
+  },
+  buttonText: {
+    color: "white",
+    textAlign: "center",
+  },
+});
--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -4,7 +4,13 @@ import PrimaryButton from "../components/PrimaryButton";
 export default function StartGameScreen() {
   return (
     <View style={styles.inputContainer}>
-      <TextInput></TextInput>
+      <TextInput
+        style={styles.numberInput}
+        maxLength={2}
+        keyboardType="number-pad"
+        autoCapitalize="none"
+        autoCorrect={false}
+      ></TextInput>
       <PrimaryButton>Reset</PrimaryButton>
       <PrimaryButton>Confirm</PrimaryButton>
     </View>
@@ -13,8 +19,26 @@ export default function StartGameScreen() {
 
 const styles = StyleSheet.create({
   inputContainer: {
-    flex: 1,
     padding: 16,
     marginTop: 100,
+    marginHorizontal: 24,
+    borderRadius: 8,
+    backgroundColor: "#4e0329",
+    elevation: 4,
+    shadowColor: "black",
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 6,
+    shadowOpacity: 0.25,
+  },
+  numberInput: {
+    height: 50,
+    width: 50,
+    textAlign: "center",
+    fontSize: 32,
+    borderBottomColor: "#ddb52f",
+    borderBottomWidth: 2,
+    color: "#ddb52f",
+    marginVertical: 8,
+    fontWeight: "bold",
   },
 });
