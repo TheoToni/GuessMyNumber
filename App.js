55e4b86384c3570ab6437e30ee61ceee2f801711
TheoToni <32997269+TheoToni@users.noreply.github.com>
2024-10-28 16:59:28 +0100

    Switching Screens

--- App.js
+++ App.js
@@ -1,9 +1,23 @@
 import { StyleSheet } from "react-native";
+import { useState } from "react";
 import StartGameScreen from "./screens/StartGameScreen";
+import GameScreen from "./screens/GameScreen";
 import { LinearGradient } from "expo-linear-gradient";
 import { ImageBackground } from "react-native";
 
 export default function App() {
+  const [userNumber, setUserNumber] = useState();
+
+  function pickedNumberHandler(pickedNumber) {
+    setUserNumber(pickedNumber);
+  }
+
+  let screen = <StartGameScreen onPickNumber={pickedNumberHandler} />;
+
+  if (userNumber) {
+    screen = <GameScreen />;
+  }
+
   return (
     <LinearGradient colors={["#4e0329", "#ddb52f"]} style={styles.container}>
       <ImageBackground
@@ -12,7 +26,7 @@ export default function App() {
         style={styles.container}
         imageStyle={styles.backgroundImage}
       >
-        <StartGameScreen />
+        {screen}
       </ImageBackground>
     </LinearGradient>
   );
--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -2,7 +2,7 @@ import { TextInput, Pressable, View, StyleSheet, Alert } from "react-native";
 import PrimaryButton from "../components/PrimaryButton";
 import { useState } from "react";
 
-export default function StartGameScreen() {
+export default function StartGameScreen({ onPickNumber }) {
   const [enteredNumber, setEnteredNumber] = useState("");
 
   function numberInputHandler(enteredText) {
@@ -23,7 +23,7 @@ export default function StartGameScreen() {
       );
       return;
     }
-    console.log("valid number");
+    onPickNumber(chosenNumber);
   }
 
   return (
