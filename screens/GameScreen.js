e8fe26202ddf337bf8bc4b73ad13df260a3783e6
TheoToni <32997269+TheoToni@users.noreply.github.com>
2024-10-31 16:11:03 +0100

    Styling logged items

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -6,7 +6,7 @@ import Card from "../components/ui/Card";
 import InstructionText from "../components/ui/InstructionText";
 import PrimaryButton from "../components/ui/PrimaryButton";
 import Title from "../components/ui/Title";
-
+import GuessLogItem from "../components/game/GuessLogItem";
 function generateRandomBetween(min, max, exclude) {
   const rndNum = Math.floor(Math.random() * (max - min)) + min;
 
@@ -63,6 +63,8 @@ function GameScreen({ userNumber, onGameOver }) {
     setGuessRounds((prevGuessRounds) => [newRndNumber, ...prevGuessRounds]);
   }
 
+  const guessRoundsListLength = guessRounds.length;
+
   return (
     <View style={styles.screen}>
       <Title>Opponent's Guess</Title>
@@ -86,7 +88,12 @@ function GameScreen({ userNumber, onGameOver }) {
       </Card>
       <FlatList
         data={guessRounds}
-        renderItem={(itemData) => <Text>{itemData.item}</Text>}
+        renderItem={(itemData) => (
+          <GuessLogItem
+            roundNumber={guessRoundsListLength - itemData.index}
+            guess={itemData.item}
+          />
+        )}
         keyExtractor={(item) => item}
       ></FlatList>
     </View>
